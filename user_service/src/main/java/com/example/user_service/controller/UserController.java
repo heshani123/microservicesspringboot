a4bb968f4662876c4a799143c151ed678d29bc7c
yehan@2000 <heshanishehara510@gmail.com>
2023-11-30 19:33:42 +0530

    update

---
+++ user_service/src/main/java/com/example/user_service/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/save")
+    public String saveUser(){
+        return "saved user!";
+    }
+}
