10ceda1de65b09a7bd8c99fc4161310616b4b2a5
yehan@2000 <heshanishehara510@gmail.com>
2023-11-30 16:10:06 +0530

    Initial commit

---
+++ src/test/java/com/example/microservices_springboot/MicroservicesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
